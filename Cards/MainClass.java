d127202672b5413555d5fc5436cef61cab27cdf0
nagalil-honaj-hteal3 <96222174+nagalil-honaj-hteal3@users.noreply.github.com>
2022-02-23 19:00:38 -0800

    Update MainClass.java

--- Cards/MainClass.java
+++ Cards/MainClass.java
@@ -1,47 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author elaet
- */
-//Created by Benno W, Lily B, Elaeth L.
 package cards;
-import java.awt.*;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.Dimension;
-import javax.swing.*;//includes the ImageIcon and button
-import java.util.Random;//Randomize the cards when shuffling
-//import javax.swing.ImageIcon;//importing poker card images
-public class MainClass extends JPanel{
-//import java.awt.Color;
-    
-}
-	/*
-	public class Button{  
-		public static void main(String[] args) {  
-		    JFrame f = new JFrame("Button Example");  
-		    final JTextField tf = new JTextField();  
-		    tf.setBounds(50,50, 150,20);  
-		    JButton b=new JButton("Shuffle");  
-		    b.setBounds(50,100,95,30);  
-		    b.addActionListener(new ActionListener(){  
-		public void actionPerformed(ActionEvent e){  
-		            tf.setText("Welcome to Javatpoint.");  
-		        }  
-		    });  
-		    f.add(b);f.add(tf);  
-		    f.setSize(400,400);  
-		    f.setLayout(null);  
-		    f.setVisible(true);   
-			}  
-		}  
-	
-	public void paintIcon(Component c, Graphics g, int x, int y){
-		Poker.paintIcon(c, g, x, y);
-	}
-	*/
+public class MainClass {
 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        cards p = new cards(); //call Poker constructor to create green window and button
+    }
+}
